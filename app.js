f384ea9165ea24d1d4ad79b72cc885fa5e5d3f08
TheStevenTL <torrezlopez456@gmail.com>
2025-11-30 13:12:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function irLogin() { window.location.href = "IniciarSesion.html"; }
+function irRegister() { window.location.href = "Registro.html"; }
+function irPerfil() { window.location.href = "Perfil.html"; }
+function irJuegos() { window.location.href = "Juegos.html"; }
+function irHome() { window.location.href = "ProyectoWeb.html"; }
